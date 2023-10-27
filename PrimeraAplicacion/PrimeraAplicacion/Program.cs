15d40a122aaa69794a876a3af75f87c8ac735f4b
lilivanlainez <118385439+lilivanlainez@users.noreply.github.com>
2023-10-26 22:30:28 -0600

    26102023

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//estamos aprendiendo a programar en  C#
 namespace PrimeraAplicacion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");  /*esto imprime */
         }
     }
 }
