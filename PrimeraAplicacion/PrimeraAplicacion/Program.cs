77993d05313b7af71138523b1ae4773142fb144a
lilivanlainez <118385439+lilivanlainez@users.noreply.github.com>
2023-11-02 12:10:04 -0600

    02102023

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -11,6 +11,12 @@ namespace PrimeraAplicacion
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");  /*esto imprime */
+            
+            int edad=28;
+
+
+            Console.WriteLine(edad);
+        
         }
     }
 }
