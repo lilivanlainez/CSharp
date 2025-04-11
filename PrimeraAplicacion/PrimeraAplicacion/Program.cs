022ac5b6842e7979af78f19d134c32283c0ddf8f
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-04-11 16:54:11 -0600

    110420254

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -10,41 +10,12 @@ namespace PrimeraAplicacion
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-            int edadPersona1;
-            int edadPersona2;
-            int edadPersona3;
-            int edadPersona4;
-
-            edadPersona1 = edadPersona2 = edadPersona3 = edadPersona4 = 27;
-
-            Console.WriteLine(edadPersona1);
-
-            var edadPersona = 34;
-
-            Console.WriteLine(edadPersona);
-
-            var miNombre = "Nelson Ivan Colorado Lainez";
-            Console.WriteLine(miNombre);
-
-            double temperatura = 30.4;
-            Console.WriteLine(temperatura);
-            int temperaturaMadrid;
-
-            temperaturaMadrid =(int) temperatura;
-            Console.WriteLine(temperaturaMadrid);
-
-            int habitantesCiudades = 1000;
-            long habitantesCiudad2018 = habitantesCiudades;
+            Console.WriteLine("Introduce el primer valor numerico");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + " es el valor introducido");
+            
 
 
-=======
-            Console.WriteLine(7*5);
-            int edad = 19;
-            Console.WriteLine("Tienes unda edad de: " + 19);
-            Console.WriteLine($"Tienes una edad de: {19} anos");
-
->>>>>>> Stashed changes
         }
     }
 }
