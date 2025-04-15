81f50c06e91337adb3a71ff0c38f5408bd872979
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-04-14 22:45:36 -0600

    14042025

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -9,35 +9,13 @@ namespace PrimeraAplicacion
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
-            Console.WriteLine(restaNumeros(5,6));
             
-            void mensajeEnPantalla()
-            {
-                Console.WriteLine("Mensaje desde el metodo");
-            }
-            int sumaNumeros()
-            {
-                int num1 = 7;
-                int num2 = 7;
-
-                int resultado = num1 + num2;
-                return resultado;
-            }
-
-            Console.WriteLine(sumaNumeros());
-            mensajeEnPantalla();
-
-            
-
-
-
         }
-        private static int restaNumeros(int num1, int num2)
-        {
-            return num2 - num1;
-        }
+        static int suma(int operador, int operador2) => operador + operador2;
 
+        static int suma(int numero1, int numero2, int numero3)=> numero1 + numero2 + numero3;
     }
 }
