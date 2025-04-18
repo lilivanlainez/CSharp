1473dd785476eb50b7d53fec51598eb695702a5c
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-04-18 13:42:11 -0600

    18042025

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -13,30 +13,32 @@ namespace PrimeraAplicacion
         static void Main(string[] args)
         {
             int edad;
+            string carnet;
+            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
             Console.WriteLine("Introduce tu edad");
             edad = Int32.Parse(Console.ReadLine());
-
-            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
 
             if(edad >= 18)
             {
                 Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
+                Console.WriteLine("Tienes Licencia de conducir?");
+                carnet = Console.ReadLine();
+                if (edad >= 18 && carnet == "si")
+                {
+                    Console.WriteLine("Puedes conducir");
+                }
+                else
+                {
+                    Console.WriteLine("Lo  siento, no puedes conducir");
+                }
             }
             else
             {
                 Console.WriteLine("No puedes pasar, regresa cuando seas mayor de edad");
             }
 
-            Console.WriteLine("Tienes Licencia de conducir?");
-            string carnet= Console.ReadLine();
-
-            if(edad>=18 && carnet == "si")
-            {
-                Console.WriteLine("Puedes conducir");
-            }else
-            {
-                Console.WriteLine("Lo  siento, no puedes conducir");
-            }
+            
+            
 
         }
 
