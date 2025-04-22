4a9be328f6cc69c86194c7bb21430bc1fb075b83
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-04-22 12:18:25 -0600

    22042025

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -12,32 +12,25 @@ namespace PrimeraAplicacion
     
         static void Main(string[] args)
         {
-            int edad;
-            string carnet;
-            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
-            Console.WriteLine("Introduce tu edad");
-            edad = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe la hora");
 
-            if(edad >= 18)
-            {
-                Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
-                Console.WriteLine("Tienes Licencia de conducir?");
-                carnet = Console.ReadLine();
-                if (edad >= 18 && carnet == "si")
-                {
-                    Console.WriteLine("Puedes conducir");
-                }
-                else
-                {
-                    Console.WriteLine("Lo  siento, no puedes conducir");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No puedes pasar, regresa cuando seas mayor de edad");
-            }
+            int hora = int.Parse(Console.ReadLine());
 
-            
+            switch (hora)
+            {
+                case int n when n<12:
+                    Console.WriteLine("Buenos dias");
+                    break;
+                case int n when n > 12 && n <18:
+                    Console.WriteLine("Buenas Tardes");
+                    break;
+                case int n when n > 18 && n < 24:
+                    Console.WriteLine("Buenas noches");
+                    break;
+                default:
+                    Console.WriteLine("No se que horas son");
+                    break;
+            }
             
 
         }
