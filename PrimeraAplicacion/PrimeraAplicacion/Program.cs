d5e11bb80534adc8a486a34d24694d4c25c1f325
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-04-24 06:42:23 -0600

    24042025

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -12,31 +12,23 @@ namespace PrimeraAplicacion
     
         static void Main(string[] args)
         {
-            int edad;
-            string carnet;
-            Console.WriteLine("Vamos a evaluar si eres mayor de edad");
-            Console.WriteLine("Introduce tu edad");
-            edad = Int32.Parse(Console.ReadLine());
 
-            if(edad >= 18)
-            {
-                Console.WriteLine("Adelante puedes pasar porque eres mayor de edad");
-                Console.WriteLine("Tienes Licencia de conducir?");
-                carnet = Console.ReadLine();
-                if (edad >= 18 && carnet == "si")
-                {
-                    Console.WriteLine("Puedes conducir");
-                }
-                else
-                {
-                    Console.WriteLine("Lo  siento, no puedes conducir");
-                }
-            }
-            else
-            {
-                Console.WriteLine("No puedes pasar, regresa cuando seas mayor de edad");
-            }
+            int a=0;
 
+            while (a <30)
+            {
+                Console.WriteLine("El numero es: "+ a);
+                a++;
+            }
+            Random rnd = new Random();
+            int numeroAleatorio = rnd.Next(0,100);
+
+            while(numeroAleatorio != 50)
+            {
+                Console.WriteLine(numeroAleatorio);
+               
+                numeroAleatorio = rnd.Next(0,100);
+            }
             
             
 
