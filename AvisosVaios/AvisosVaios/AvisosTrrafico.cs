55298a92d64e5ad041341dd5d88784da426d1fda
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-08-11 22:55:51 -0600

    12082025

---
+++ AvisosVaios/AvisosVaios/AvisosTrrafico.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisosVaios
+{
+    internal class AvisosTrrafico : IAvisos
+    {
+        public AvisosTrrafico()
+        {
+            remitente = "Dirección General de Tráfico";
+            mensaje = "Hay un accidente en la carretera A-1, por favor evite la zona.";
+            fecha = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+        public AvisosTrrafico(string remitente, string menaje, string fecha)
+        {
+            this.remitente = remitente;
+            this.mensaje = menaje;
+            this.fecha = fecha;
+        }
+        public void mostrarAviso()
+        {
+            Console.WriteLine("Mensaje {0} he sido enviado por {1} el dia {2}", mensaje, remitente, fecha);
+        }
+        public string getFecha()
+        {
+            return DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private string remitente;
+        private string mensaje;
+        private string fecha;
+       
+    }
+}
---
+++ AvisosVaios/AvisosVaios/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AvisosVaios;
+
+namespace AvisosVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AvisosTrrafico aviso1 = new AvisosTrrafico();
+            aviso1.mostrarAviso();
+            AvisosTrrafico aviso2 = new AvisosTrrafico("Ayuntamiento de Madrid", "Corte de tráfico en la calle Mayor por evento deportivo", "15/10/2023");
+            aviso2.mostrarAviso();
+        }
+    }
+}
