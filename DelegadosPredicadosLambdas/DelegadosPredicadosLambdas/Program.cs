342e39673f0613059caf4e7142250311d3afca7d
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-09-14 16:43:25 -0600

    14092025

---
+++ DelegadosPredicadosLambdas/DelegadosPredicadosLambdas/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DelegadosPredicadosLambdas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //creacion del objeto delegado
+            ObjetoDelegado ElDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
+            ElDelegado("Ivan");
+
+            ElDelegado= new ObjetoDelegado(MensajeDespedida.SaludoDespedida);
+            ElDelegado("Ivan");
+        }
+        //definicion del objeto delegado
+        delegate void ObjetoDelegado(string msj);
+    }
+
+    class MensajeBienvenida
+    {
+        public static void SaludoBienvenida(string msj)
+        {
+            Console.WriteLine($"Hola {msj} acabo de llegar, que tal?");
+        }
+    }
+    class MensajeDespedida
+    {
+        public static void SaludoDespedida(string msj)
+        {
+            Console.WriteLine($"Hola ya me marcho, Ccuidate mucho {msj}");
+        }
+    }
+}
