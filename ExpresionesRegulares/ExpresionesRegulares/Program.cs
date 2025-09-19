c8d9d05a6a04ca48ad10c4a644f1f5dee0face72
Ivan Lainez <118385439+lilivanlainez@users.noreply.github.com>
2025-09-18 21:32:25 -0600

    18092025

---
+++ ExpresionesRegulares/ExpresionesRegulares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExpresionesRegulares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String frase = "Mi nombre es Ivan y mi numero de telefono es (+503) 71179004 y mi codigo postal es 503";
+
+            string patron = "[I]";
+
+            Regex miRegex = new Regex(patron);
+
+            MatchCollection matches = miRegex.Matches(frase);
+            if (matches.Count > 0) Console.WriteLine("Se ha encontrado una I");
+            else Console.WriteLine("No se ha encontrado una J");
+            
+            
+            
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+
+        }
+    }
+}
